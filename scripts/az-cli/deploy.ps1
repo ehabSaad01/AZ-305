1a46f74ae62bab52e1bff6d45657151391ba263a
ehabsaad01 <ehab.saad100985@gmail.com>
2026-02-04 11:10:02 +0100

    Add Azure CLI deployment script for monitoring baseline

---
+++ scripts/az-cli/deploy.ps1
@@ -0,0 +1,9 @@
+﻿# Deploy baseline monitoring (Log Analytics)
+
+az group create `
+  --name rg-az305-monitoring-weu `
+  --location westeurope
+
+az deployment group create `
+  --resource-group rg-az305-monitoring-weu `
+  --template-file infra/bicep/main.bicep
